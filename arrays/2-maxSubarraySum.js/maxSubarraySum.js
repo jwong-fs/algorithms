8f152125a3ad30b533aeaa697460572a4188ef57
Jenny Wong <jennyw981@gmail.com>
2021-12-27 23:21:02 -0500

    add maxSubarraySum problem

---
+++ arrays/2-maxSubarraySum.js/maxSubarraySum.js
@@ -0,0 +1,8 @@
+// Implement a function that accepts an array of integers and a number called n.
+// The function should calculate the maximum sum of n consecutive elements in the array.
+
+const maxSubarraySum = (arr) => {
+
+}
+
+module.exports = maxSubarraySum;
