2a71dcffd0c24f18868b1ec17b519700bb7ae97f
Jenny Wong <jennyw981@gmail.com>
2021-12-30 22:02:41 -0500

    add MaxSubsetSumNoAdjacent problem

---
+++ dynamicProgramming/0-maxSubsetSumNoAdjacent/maxSubsetSumNoAdjacent.js
@@ -0,0 +1,2 @@
+// Write a function that takes in an array of positive integers and returns the maximum sum of non-adjacant elements in the array.
+// If the input array is empty, the function should return 0.
